37a52a7526f9e0a75f6b05031e9daf5c8ff59074
jacobtie <jacobtie@gmail.com>
2019-03-28 23:45:21 -0400

    Added in-line documentation to Go and JavaScript code

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ import (
 	"strings"
 )
 
+// Helper function to render template with base layout
 func renderTemplate(w http.ResponseWriter, filename string, data map[string]interface{}) {
 	t, _ := template.ParseFiles("base.html", filename)
 	t.ExecuteTemplate(w, "base", data)
 }
 
+// Helper function to forward any unknown routes to 404 page
 func makeHandler(fn func(http.ResponseWriter, *http.Request), route string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if len(r.URL.Path[len(route):]) > 1 {
@@ -25,25 +27,35 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request), route string) http
 	}
 }
 
+// Renders home page
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html", nil)
 }
 
+// Renders about page
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.html", nil)
 }
 
+// Renders and handles interpreter page
 func interpretHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
+		// Renders normal interpreter page
 		renderTemplate(w, "interpreter.html", nil)
 	case http.MethodPost:
+		// Parses data from form
 		r.ParseForm()
+		// Extracts source code
 		program := r.FormValue("source")
+		// Compiles and runs program with Google's endpoint to get output/error
 		message, err := compileProgram(program)
+		// Replaces line breaks in message with HTML line breaks
 		message = strings.Replace(message, "\n", "\n<br />", -1)
+		// Renders template with correct data
 		renderTemplate(w, "interpreter.html", map[string]interface{}{"Program": program, "Message": template.HTML(message), "Error": err})
 	default:
+		// Renders normal interpreter page in case a different HTTP method is used
 		renderTemplate(w, "interpreter.html", nil)
 	}
 }
@@ -77,8 +89,8 @@ func compileProgram(program string) (string, bool) {
 
 	// Does the request, closes the request, and extracts the body
 	res, _ := client.Do(req)
-	defer res.Body.Close() // Runs after function finishes
 	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close() // Runs after function finishes
 
 	// Maps the output of the body to the data map
 	var data map[string]string
@@ -91,9 +103,12 @@ func compileProgram(program string) (string, bool) {
 	return string(data["compile_errors"]) + string(data["output"]), compErr
 }
 
+// Entry point of the program
 func main() {
+	// Sets up routes
 	http.HandleFunc("/", makeHandler(mainHandler, "/"))
 	http.HandleFunc("/about", makeHandler(aboutHandler, "/about"))
 	http.HandleFunc("/interpret", makeHandler(interpretHandler, "/interpret"))
+	// Starts the server
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
