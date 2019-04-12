a9caaf5042c7f7e4bdebf63c27c260b2c04edcc7
jacobtie <jacobtie@gmail.com>
2019-04-12 17:34:56 -0400

    Added if sample program

---
+++ samples/if.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 5
+	if x == 5 {
+		fmt.Println("This code will run if x is equal to 5")
+	} else {
+		fmt.Println("This code will not run if x is equal to 5")
+	}
+}
